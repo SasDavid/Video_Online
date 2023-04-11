a0eed19b92eb5d9f3b9d90b87999d581c44c1ad1
DavidDeivis <espontaneoclips@gmail.com>
2023-04-11 18:29:01 -0400

    seis

--- server.js
+++ server.js
@@ -85,9 +85,10 @@ io.on('connection', (socket)=>{
 			for (let i = 0; i < usuariosRegister.length; i++) {
 				if(usuariosRegister[i].clave == data.clave){
 					usuariosRegister[i].id = socket;
-					io.emit("login", usuariosClient());
 				}
 			}
+
+			io.emit("login", usuariosClient());
 		}
 		
 	})
